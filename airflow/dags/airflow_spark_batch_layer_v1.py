9300d2241e4617d755e127037a2b2b58f4f68bdf
Mikel Emaldi <m.emaldi@deusto.es>
2025-05-05 11:35:29 +0200

    Airflow deleting S3 objects

--- airflow/dags/airflow_spark_batch_layer_v1.py
+++ airflow/dags/airflow_spark_batch_layer_v1.py
@@ -2,6 +2,8 @@ from airflow.models import DAG
 from datetime import datetime, timedelta
 from airflow.operators.bash import BashOperator
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from airflow.providers.amazon.aws.operators.s3 import S3DeleteObjectsOperator
+import os
 
 with DAG(
     dag_id="spark_batch_layer_v1",
@@ -13,21 +15,27 @@ with DAG(
         "retries": 0,
         "retry_delay": timedelta(minutes=5)
     },
-    schedule="*/10 * * * *",
+    schedule="00 * * * *",
     start_date=datetime(2023, 1, 1),
     catchup=False,
     tags=["example", "TGVD"],
 ) as dag:
-    bash_task = BashOperator(
-        task_id="rm_output",
-        bash_command="rm -rf /opt/spark/work-dir/spark-batch-output/output"
+    # bash_task = BashOperator(
+    #     task_id="rm_output",
+    #     bash_command="rm -rf /opt/spark/work-dir/spark-batch-output/output"
+    # )
+
+    delete_objects = S3DeleteObjectsOperator(
+        task_id="delete_output",
+        bucket="spark-batch-output",
+        prefix="output",
     )
 
     spark_conf = {
         "spark.jars.ivy": "/opt/airflow/",
         "spark.jars.packages": "org.apache.hadoop:hadoop-aws:3.3.4",
-        "spark.hadoop.fs.s3a.access.key": "minio-root-user",
-        "spark.hadoop.fs.s3a.secret.key": "minio-root-password",
+        "spark.hadoop.fs.s3a.access.key": os.environ.get("AWS_ACCESS_KEY_ID"),
+        "spark.hadoop.fs.s3a.secret.key": os.environ.get("AWS_SECRET_ACCESS_KEY"),
         "spark.hadoop.fs.s3a.endpoint": "http://minio:9000",
         "spark.hadoop.fs.s3a.path.style.access": "true"
     }
@@ -38,5 +46,5 @@ with DAG(
         conf=spark_conf
     )
 
-    bash_task >> submit_job
+    delete_objects >> submit_job
 
